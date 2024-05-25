1ec05b51c5d89a423d860d7ddbbbcb9b9b8acb9b
Sukansh Gupta <sukansh.07@gmail.com>
2024-05-25 22:55:39 +0530

    Made a song player ui which has a add to fav button and favourites tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,80 @@
-import React from 'react';
-import Card from './Components/Card';
-import Songs from './Components/Songs';
+import React, { useState } from "react";
+import Card from "./Components/Card";
+import Navbar from "./Components/Navbar";
 
 function App() {
+  const data = [
+    {
+      image:
+        "https://i.pinimg.com/474x/cf/bc/9a/cfbc9aa23d256555733494a62ddcac9a.jpg",
+      name: "Balle Balle",
+      Artist: "SukkuGod",
+      added: false,
+    },
+    {
+      image:
+        "https://i.pinimg.com/236x/4e/02/43/4e02439c86bf5ea40a2048f114b985e3.jpg",
+      name: "Challenger",
+      Artist: "Eminem",
+      added: false,
+    },
+    {
+      image:
+        "https://i.pinimg.com/236x/e5/3b/2a/e53b2aa902156191c38bdb24ee311466.jpg",
+      name: "HoneyBeez",
+      Artist: "Honey Singh",
+      added: false,
+    },
+    {
+      image:
+        "https://i.pinimg.com/236x/7b/b6/bc/7bb6bc5208f5830cab8829649d6de241.jpg",
+      name: "Another Love",
+      Artist: "Tom Odell",
+      added: false,
+    },
+    {
+      image:
+        "https://i.pinimg.com/236x/50/68/25/50682586781195e2c080493ede9ae930.jpg",
+      name: "Dreamer",
+      Artist: "Mohit Chauhan",
+      added: false,
+    },
+    {
+      image:
+        "https://i.pinimg.com/236x/20/f3/eb/20f3ebe8c8a1540739046f6e3fabfce7.jpg",
+      name: "Alone",
+      Artist: "Weeknd",
+      added: false,
+    },
+    {
+      image:
+        "https://i.pinimg.com/236x/66/98/10/669810ffde2d068f6d8484a25001754f.jpg",
+      name: "After Dark",
+      Artist: "Mr. Kitty",
+      added: false,
+    },
+  ];
+
+  const [songData, setSongData] = useState(data);
+  const handleClick = (index) => {
+    setSongData((prev) => {
+      return prev.map((item, itemindex) => {
+        if(index === itemindex) return {...item, added: !item.added};
+        return item;
+      })
+    })
+  }
+
   return (
-    <div>
-      <Songs />
+    <div className="w-full h-screen bg-zinc-300">
+      <Navbar data={songData} />
+      <div className="px-20 flex gap-10 mt-10 flex-wrap">
+        {songData.map((obj, index) => (
+          <Card data={obj} handleClick={handleClick} index={index} key={index} />
+        ))}
+      </div>
     </div>
   );
 }
+
 export default App;
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,29 +1,23 @@
-import React from 'react'
+import React from "react";
 
-function Card() {
+function Card({ data, handleClick, index }) {
 
-  const data = [
-    {image: 'https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?q=80&w=1796&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', name: "Amazon Basics", description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat in necessitatibus ullam!"},
-    {image: 'https://plus.unsplash.com/premium_photo-1664110691129-ca0f4fbe8533?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZGFpbHklMjBvYmplY3RzfGVufDB8fDB8fHww', name: "Daily Objects", description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat in necessitatibus ullam!"},
-    {image: 'https://images.unsplash.com/photo-1570913149827-d2ac84ab3f9a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXBwbGV8ZW58MHx8MHx8fDA%3D', name: "Apples", description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat in necessitatibus ullam!"},
-  ]
-
+const {image, Artist, name, added} = data;
 
   return (
-    <div className='w-full h-screen flex items-center justify-center gap-10 bg-zinc-200'>
-      {data.map((elem, index) => (
-        <div key={index} className="w-52 bg-zinc-100 left-1/2 top-1/2 rounded-md overflow-hidden">
-        <div className="w-full h-32 bg-zinc-300">
-          <img className='w-full h-full object-cover' src={elem.image} alt="" />
-        </div>
-        <div className="w-full px-3 py-4">
-          <h2 className="font-semibold">{elem.name}</h2>
-          <p className="text-xs mt-3">{elem.description}</p>
-        </div>
+    <div className="w-60 rounded-md bg-zinc-100 p-4 flex gap-4 pb-8 mt-10 relative">
+      <div className="w-20 h-20 bg-orange-600 rounded-md overflow-hidden">
+        <img className="w-full h-full object-cover" src={image} alt="" />
       </div>
-      ))}
+      <div>
+        <h3 className="text-xl leading-none whitespace-nowrap font-semibold">{name}</h3>
+        <h6 className="text-sm">{Artist}</h6>
+      </div>
+      <button onClick={() => handleClick(index)} className={`px-4 py-3 rounded-full whitespace-nowrap ${added === false ? "bg-orange-600" : "bg-teal-600"} text-white absolute bottom-0 left-1/2 -translate-x-[50%] translate-y-[50%] text-xs`}>
+        {added === false ? "Add to Favourites" : "Added"}
+      </button>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
